585abf384eb3b09533e245014ebd58297739dd8e
karthiksaiphanindra <58380881+karthiksaiphanindra@users.noreply.github.com>
2019-12-06 09:39:23 +0530

    Create C_program.c

---
+++ C_program.c
@@ -0,0 +1,11 @@
+##for finding the max and min vlaues of the interger
+#include <stdio.h>
+#include <limits.h>
+void main()
+{
+    int a,b;
+    a=INT_MAX;
+    printf("%d\n",a);
+    b=INT_MIN;
+    printf("%d",b);
+ }
