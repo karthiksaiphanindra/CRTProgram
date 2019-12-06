409ba9046b140e513f10d8fdd64e07ab5d27b6c7
karthiksaiphanindra <58380881+karthiksaiphanindra@users.noreply.github.com>
2019-12-06 11:30:25 +0530

    Update C_program.c

--- C_program.c
+++ C_program.c
@@ -90,4 +90,133 @@ void main()
     int a=29;
     printf("%d",a);
 }
-///////////////////////
+////////////////////////////////
+to print the value of the char
+#include <stdio.h>
+void main()
+{
+    int a;
+   a = printf("hello\n");
+    printf("%d",a);
+}
+///////////////////////////////
+#include <stdio.h>
+void main()
+{
+    int a,b,c,d;
+   c=scanf("%d%d",&a,&b,&d);
+    printf("%d",c);
+}
+//////////////////////////////
+or operator
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=6,c;
+    c=b||a;
+    printf("%d",c);
+}
+/////////////////////////////
+and operator
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=6,c;
+    c=b&&a;
+    printf("%d",c);
+}
+/////////////////////////////
+or and operator
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=6,c,d=-7;
+    c=b||a&&d;
+    printf("%d",c);
+}
+////////////////////////////
+not operator
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,c;
+    c=!a
+    printf("%d",c);
+}
+////////////////////////////
+realtion operator ==
+    #include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=5,c;
+    if(a==b)
+    printf("yes");
+    else
+    printf("no");
+}
+///////////////////////////
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=6,c;
+    if(a=b)
+    printf("yes");
+    else
+    printf("no");
+    printf("%d",a);
+}
+/////////////////////////////
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=6,c;
+    if(a)
+    printf("yes");
+    else
+    printf("no");
+   printf("%d",a);
+}
+//////////////////////////////
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=0,c;
+    if(a!=b)
+    printf("yes");
+    else
+    printf("no");
+   printf("%d",a);
+}
+///////////////////////////////
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=0,c;
+    if(a<b)
+    printf("yes");
+    else
+    printf("no");
+   printf("%d",a);
+}
+//////////////////////////////
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a=5,b=0,c;
+    if(a>b)
+    printf("yes");
+    else
+    printf("no");
+   printf("%d",a);
+}
+/////////////////////////////
